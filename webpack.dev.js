5dcac7eab134336ebf907fdaa9593bc80a50f706
Begerad, Stefan <stefan@begerad.de>
2021-09-09 12:32:25 -0400

    feat: added migration example according to https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,3 +11,36 @@ module.exports = merge(common, {
         port:55555
     },
 });
+
+///webpack v3
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    contentBasePublicPath: "/serve-content-base-at-this-url",
+    serveIndex: true,
+    watchContentBase: true,
+    watchOptions: {
+      poll: true,
+    },
+  },
+};
+
+//webpack v4
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "static"),
+      staticOptions: {},
+      // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+      // Can be:
+      // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+      publicPath: "/static-public-path/",
+      // Can be:
+      // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+      serveIndex: true,
+      // Can be:
+      // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+      watch: true,
+    },
+  },
+};
